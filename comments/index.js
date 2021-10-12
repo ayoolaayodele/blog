762737f22f2ae19b1204f166ed25397736c90506
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-10-12 05:26:57 -0700

    Added comment Moderation to comment service to emit an event to event-bus

--- comments/index.js
+++ comments/index.js
@@ -35,8 +35,32 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event Received', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, comment } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    //We update pending to new status//coming from line 45
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
