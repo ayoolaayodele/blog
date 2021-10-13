594b8d9e27e34190cae3c03ab98500b8b0c9a3f4
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-10-13 06:49:12 -0700

    Solved issue of missing events by storing all data type in event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,39 +2,32 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-
 app.use(express.json());
 
-app.post('/events', async (req, res) => {
+const events = [];
+
+app.post('/events', (req, res) => {
   const event = req.body;
-  //post service
-  try {
-    await axios.post('http://localhost:4000/events', event);
-    res.send({ status: 'ok' });
-  } catch (err) {
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
-  }
-  //comment service
-  try {
-    await axios.post('http://localhost:4001/events', event);
-    res.send({ status: 'ok' });
-  } catch (err) {
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
-  }
-  //query service
-  try {
-    await axios.post('http://localhost:4002/events', event);
-    res.send({ status: 'ok' });
-  } catch (err) {
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log(err.message);
-  }
-  //moderation service
-  try {
-    await axios.post('http://localhost:4003/events', event);
-    res.send({ status: 'ok' });
-  } catch (err) {
+  });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
     console.log(err.message);
-  }
+  });
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
